fe69ac9f6e32f4c0d80120274dd0da802a0dcb21
lopez <usts035619@ugb.edu.sv>
2020-09-03 22:29:53 -0600

    agregue el tabhost

--- app/src/main/java/com/example/programacion_lll/MainActivity.java
+++ app/src/main/java/com/example/programacion_lll/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.programacion_lll;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost pestana = (TabHost)findViewById(R.id.parcial);
+        pestana.setup();
+
+        //pestana 1
+        TabHost.TabSpec spec = pestana.newTabSpec ("Pestana Uno");
+        spec.setContent(R.id.Universal);
+        spec.setIndicator("Pestana Uno");
+        pestana.addTab (spec);
+
+        //pestana 2
+        spec = pestana.newTabSpec("Pestana Dos");
+
+        spec.setContent(R.id.Area);
+        spec.setIndicator("Pestana Dos");
+        pestana.addTab (spec);
     }
 }
