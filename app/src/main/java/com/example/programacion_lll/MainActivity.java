b2c8f650e164cc96ccc7e99213f3d35a466d6510
lopez <usts035619@ugb.edu.sv>
2020-09-27 21:58:50 -0600

    tienda

--- app/src/main/java/com/example/programacion_lll/MainActivity.java
+++ app/src/main/java/com/example/programacion_lll/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.programacion_lll;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+
 }
+
